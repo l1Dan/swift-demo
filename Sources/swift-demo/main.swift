71ab7deaa76cefd7bc6827d98f15b541bfe87064
Leo Lee <l1dan@hotmail.com>
2018-10-19 00:41:39 +0800

    修改配置文件

--- Sources/swift-demo/main.swift
+++ Sources/swift-demo/main.swift
@@ -14,3 +14,15 @@ debugPrint(computeSorceGreaterThanOrEqualTo60(scores: scores))
 func sum(integers: [Int]) -> Int {
     return 0
 }
+
+extension Array {
+    func map<T>(_ transform: (Element) -> (T)) -> [T] {
+        var result: [T] = []
+        for x in self {
+            result.append(transform(x))
+        }
+        return result
+    }
+}
+
+debugPrint([1, 4, 5, 6].map { $0 + 1 })
